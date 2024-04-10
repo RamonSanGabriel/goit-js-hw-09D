ea341fb20170354c1bd128e6728cec77bd857454
RamonSanGabriel <jeshero11@gmail.com>
2024-04-11 03:15:55 +0800

    refs object

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,17 +2,76 @@
 import flatpickr from 'flatpickr';
 // Additional styles import
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const date = new Date();
-const textInput = document.querySelector('input[type="text"]');
-const datePicker = document.getElementById('input#date-picker');
-const btn = document.querySelector('[data-start]');
-// const days = document.querySelector('[data-days]');
-// const hours = document.querySelector('[data-hours]');
-// const minutes = document.querySelector('[data-minutes]');
-// const seconds = document.querySelector('[data-seconds]');
-let ms = datePicker;
-btn.disabled = true;
+// const date = new Date();
+// const textInput = document.querySelector('input[type="text"]');
+// const dateTimePicker = document.querySelector('input#datetime-picker');
+// const btn = document.querySelector('button[data-start]');
+// const days = document.querySelector('span[data-days]');
+// const hours = document.querySelector('span[data-hours]');
+// const minutes = document.querySelector('span[data-minutes]');
+// const seconds = document.querySelector('span[data-seconds]');
+// let ms = dateTimePicker;
+
+const refs = {
+  datetimePicker: document.querySelector('input#datetime-picker'),
+  btn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+refs.btn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const startTime = Date.now();
+
+    if (selectedDate < startTime) {
+      Notify.failure('Please choose a date in the future');
+      btn.disabled = true;
+      return;
+    }
+    refs.btn.disabled = false;
+    let intervalId = null;
+
+    refs.btn.addEventListener('click', startCountdown);
+
+    function startCountdown() {
+      refs.btn.disabled = true;
+      refs.datetimePicker.disabled = true;
+
+      intervalId = setInterval(() => {
+        const currentTime = Date.now();
+
+        if (selectedDate < currentTime) {
+          clearInterval(intervalId);
+          refs.datetimePicker.disabled = false;
+          return;
+        }
+
+        const timeDifference = selectedDate - currentTime;
+        const { days, hours, minutes, seconds } = convertMs(timeDifference);
+
+        days.textContent = addLeadingZero(days);
+        hours.textContent = addLeadingZero(hours);
+        minutes.textContent = addLeadingZero(minutes);
+        seconds.textContent = addLeadingZero(seconds);
+      });
+    }
+  },
+};
+flatpickr('#datetime-picker', options);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0'); //01
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -32,10 +91,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-// console.log(Input);
-// console.log(date); // Mon Feb 07 20
